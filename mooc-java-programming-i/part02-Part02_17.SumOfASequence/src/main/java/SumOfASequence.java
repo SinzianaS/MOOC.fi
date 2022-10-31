7c99a31a0f957dacf6794829a4c78606203208ea
Sinziana Sovejanu <27846889+SinzianaS@users.noreply.github.com>
2022-10-31 20:00:34 +0200

    part 2

--- mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -1,10 +1,10 @@
 
+import static java.lang.Integer.parseInt;
 import java.util.Scanner;
 
 public class SumOfASequence {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
     }
 }
