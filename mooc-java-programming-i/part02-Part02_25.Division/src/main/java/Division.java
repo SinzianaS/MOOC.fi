b4a66cc38c9077906278fe533dc73d0c1b7ad750
Sinziana Sovejanu <27846889+SinzianaS@users.noreply.github.com>
2023-01-17 18:46:21 +0200

    added exercises

--- mooc-java-programming-i/part02-Part02_25.Division/src/main/java/Division.java
+++ mooc-java-programming-i/part02-Part02_25.Division/src/main/java/Division.java
@@ -1,8 +1,14 @@
 
 
 public class Division {
+    public static void division(int numerator, int denominator){
+        float div = (float)numerator/denominator;
+        System.out.println(div);
+    }
 
     public static void main(String[] args) {
+        division(3,5);
+        
         
         // Once you have implemented the division method, you can
         // try it out here. Fopr example division(3,5);
